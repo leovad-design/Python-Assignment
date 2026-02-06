d48740e4ff3a087bcab890072e7383a37214b089
leovad-design <vadakkumcherryleo@gmail.com>
2026-02-07 00:05:49 +0100

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dze8ooi5h/video/upload/v1770331229/Badtameez_Dil_Full_Song_HD_Yeh_Jawaani_Hai_Deewani_PRITAM_Ranbir_Kapoor_Deepika_Padukone_boxjsd.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dze8ooi5h/video/upload/v1770419108/Badtameez_Dil_Lyrics_-_Yeh_Jawaani_Hai_Deewani_Pritam_Benny_Dayal_Shefali_Alvarez_nv5pjs.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
